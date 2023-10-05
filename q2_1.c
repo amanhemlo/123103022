328a6a65e7d08bb0d456fe62e205e1e18bcb3215
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 19:47:36 +0530

    Update q2_1.c

--- q2_1.c
+++ q2_1.c
@@ -1,28 +1,38 @@
-#include <stdio.h>
-int add(int a, int b) // used to find sum
-{
-    return a + b;
-}
-int sub(int a, int b)
-{
-    return a - b;
-}
-int main()
-{
-    int a, b;
-    printf("enter 2 no.\n");
-    scanf("%d %d", &a, &b);
-    if (a > b)
-    {
-        printf("%d", add(a, b));
-    }
-    if (a < b)
-    {
-        printf("%d", sub(a, b));
-    }
-    if (a == b)
-    {
-        printf("both no. equal");
-    }
-    return 0;
+
+#include<stdio.h>
+
+int main(){
+
+	int num1,num2;
+
+	printf("ENTER FIRST NUMBER : ");
+
+	scanf("%d",&num1);
+
+	printf("ENTER SECOND NUMBER : ");
+
+	scanf("%d",&num2);
+
+	if(num1==num2){
+
+		printf("BOTH NUMBERS ARE EQUAL\n");
+
+		printf("ENTER DIFFERENT NUMBERS");
+
+	}
+
+	if(num1>num2){
+
+		printf("THE SUM OF TWO NUMBERS IS : %d",num1+num2);
+
+	}
+
+	if(num1<num2){
+
+		printf("THE DIFFERENCE OF TWO NUMBERS IS : %d",num2-num1);
+
+	}
+
+	return 0;
+
 }
