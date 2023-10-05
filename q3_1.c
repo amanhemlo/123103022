0c4b9e69e7b2047c9cd555257aa32209290122b4
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 20:01:06 +0530

    Update q3_1.c

--- q3_1.c
+++ q3_1.c
@@ -1,31 +1,47 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int factorial(int a)
-{
-    int f;
-    if (a <= 1)
-    {
-        f = 1;
-    }
-    else
-    {
-        f = a * factorial(a- 1);
-    }
-    return f;
-}
+int main(){
 
-int main()
-{
-    printf("you want to calculate factorial of?\n");
-    int a, f;
-    scanf("%d", &a);
-    f = factorial(a);
- 
-    if (a< 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-    } else {
-        printf("Factorial of %d! is %d\n", a, f);
-    }
+	int n,i,j;
 
-    return 0;
+	printf("ENTER NUMBER : ");
+
+	scanf("%d",&n);
+
+	if(n==0){
+
+		printf("FACTORIAL OF %d is : %d",n,1);
+
+	}
+
+	else if(n>0){
+
+		int fp=1;
+
+		for(i=1;i<=n;i++){
+
+			fp=fp*i;
+
+		}
+
+		printf("FACTORIAL OF %d is : %d",n,fp);
+
+	}
+
+	else{
+
+		int fn=1;
+
+		for(j=n;j<=(-1);j++){                             // LOOP RUN IN OPPOSITE DIRECTION 
+
+			fn=fn*j;                                      // LOOP RUN FROM N TO -1 AS N IS NEGATIVE 
+
+		}
+
+		printf("FACTORIAL OF %d is : %d",n,fn);
+
+	}
+
+	return 0;
+
 }
