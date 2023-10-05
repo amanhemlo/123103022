9c62173d8e4ff18595ea6c5d40219f10e6568aa3
amanhemlo <146988180+amanhemlo@users.noreply.github.com>
2023-10-05 13:41:14 +0530

    Create q2_2_alternate_sol.c

---
+++ q2_2_alternate_sol.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void greater(int a, int b, int c)
+{
+    if (a > b)
+    {
+        if (a > c)
+        {
+            printf("a is greatest\n");
+        }
+    }
+    else if (b > c)
+    {
+        if (b > a)
+        {
+            printf("b is greatest\n");
+        }
+    }
+    else
+    {
+        printf("c is greatest\n");
+    }
+}
+int main()
+{
+    int a, b, c;
+    printf("enter 3 numbers\n");
+    scanf("%d %d %d", &a, &b, &c);
+    greater(a, b, c);
+
+    return 0;
+}
