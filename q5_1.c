df867f7fb737ff3d24ab850501065830d32d59a0
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 20:16:25 +0530

    Update q5_1.c

--- q5_1.c
+++ q5_1.c
@@ -1,80 +1,143 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int day, month, year;
-    printf("Enter day: ");
-    scanf("%d", &day);
-    printf("Enter month: ");
-    scanf("%d", &month);
-    printf("Enter year: ");
-    scanf("%d", &year);
+int main(){
 
-   
-    printf("%d", day);
-    switch (day) {
-        case 1:
-        case 21:
-        case 31:
-            printf("st, ");
-            break;
-        case 2:
-        case 22:
-            printf("nd, ");
-            break;
-        case 3:
-        case 23:
-            printf("rd, ");
-            break;
-        default:
-            printf("th, ");
-            break;
-    }
+	int d,m,y;
 
-    
-    switch (month) {
-        case 1:
-            printf("Jan, ");
-            break;
-        case 2:
-            printf("Feb, ");
-            break;
-        case 3:
-            printf("Mar, ");
-            break;
-        case 4:
-            printf("April, ");
-            break;
-        case 5:
-            printf("May, ");
-            break;
-        case 6:
-            printf("Jun, ");
-            break;
-        case 7:
-            printf("Jul, ");
-            break;
-        case 8:
-            printf("Aug, ");
-            break;
-        case 9:
-            printf("Sep, ");
-            break;
-        case 10:
-            printf("Oct, ");
-            break;
-        case 11:
-            printf("Nov, ");
-            break;
-        case 12:
-            printf("Dec, ");
-            break;
-        default:
-            printf("Invalid Month, ");
-            break;
-    }
+	printf("ENTER DATE :");
 
-    
-    printf("%d\n", year);
+	scanf("%d",&d);
 
-    return 0;
+	printf("ENTER MONTH NUMBER :");
+
+	scanf("%d",&m);
+
+	printf("ENTER YEAR :");
+
+	scanf("%d",&y);
+
+	
+
+	printf("%d",d);
+
+	switch(d){
+
+		case 1:
+
+		case 21:
+
+		case 31:
+
+			printf("st, ");
+
+			break;
+
+		case 2:
+
+		case 22:
+
+		    printf("nd, ");
+
+	        break;
+
+		case 3:
+
+		case 23:
+
+		    printf("rd, ");
+
+			break;
+
+		default:
+
+		    printf("th, ");
+
+			break;	
+
+	}
+
+	
+
+	switch(m){
+
+		case 1:
+
+			printf("Jan, ");
+
+			break;
+
+		case 2:
+
+			printf("Feb, ");
+
+			break;
+
+		case 3:
+
+			printf("Mar, ");
+
+			break;
+
+		case 4:
+
+			printf("Apr, ");
+
+			break;
+
+		case 5:
+
+			printf("May, ");
+
+			break;
+
+		case 6:
+
+			printf("Jun, ");
+
+			break;
+
+		case 7:
+
+			printf("Jul, ");
+
+			break;
+
+		case 8:
+
+			printf("Aug, ");
+
+			break;
+
+		case 9:
+
+			printf("Sep, ");
+
+			break;
+
+		case 10:
+
+			printf("Oct, ");
+
+			break;
+
+		case 11:
+
+			printf("Nov, ");
+
+			break;
+
+		case 12:
+
+			printf("Dec, ");
+
+			break;
+
+	}
+
+	
+
+	printf("%d",y);
+
+	return 0;
+
 }
