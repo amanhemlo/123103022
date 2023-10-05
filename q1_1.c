9c81d3f56c26986a4fe90923af763fb758c209b8
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 19:39:18 +0530

    Update q1_1.c

--- q1_1.c
+++ q1_1.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 
-    int main(){
-    printf("enter two no.\n");
-    int a,b;
-    scanf("%d %d",&a,&b);
-    printf("sum=%d\nsub=%d\nmultiply=%d\n",a+b,a-b,a*b);
-    
+void calculator(int num1,int num2){
 
-return 0;
-    }
+	printf("SUM OF TWO NUMBER IS : %d\n",num1+num2);
+
+	printf("DIFFERENCE OF TWO NUMBER IS : %d\n",num1-num2);
+
+	printf("PRODUCT OF TWO NUMBER IS : %d\n",num1*num2);
+
+	return;
+
+}
+
+int main(){
+
+	int a,b;
+
+	printf("ENTER FIRST NUMBER : ");
+
+	scanf("%d",&a);
+
+	printf("ENTER SECOND NUMBER : ");
+
+	scanf("%d",&b);
+
+	calculator(a,b);
+
+	return 0;
+
+}
