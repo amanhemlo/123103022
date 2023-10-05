4ea1c372cb36492f87978bb7d2dc8f44e0e7567f
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 19:43:49 +0530

    Update q1_2.c

--- q1_2.c
+++ q1_2.c
@@ -1,17 +1,93 @@
-#include<stdio.h>
+#include <stdio.h>
 
-    int main(){
-    float r;
-    
-    printf("enter radius\n");
-    scanf("%f",&r);
-    float area=3.14*r*r;
-    printf("area of circle=%0.2f\n",area);
-float a;
+void circle(float r){
 
-printf("enter side of triangle\n");
-scanf("%f",&a);
-printf("area of triangle=%0.2f\nperimeter of triangle=%0.2f",(1.73/2)*a*a,3*a);
+    printf("Area of circle is:%f\n", 3.14 * r * r);
 
-return 0;
-    }
+    printf("perimeter of circle is:%f\n\n\n", 3.14 * r * 2);
+
+    return;
+
+}
+
+void triangle(float s1, float s2, float s3, float h, float b){
+
+    printf("Area of triangle is:%f\n", 0.5 * h * b);
+
+    printf("Perimeter of triangle is:%f\n\n\n", s1 + s2 + s3);
+
+    return;
+
+}
+
+void rectangle(float l, float b){
+
+    printf("Area of rectangle is:%f\n", l * b);
+
+    printf("Perimeter of rectangle is:%f\n\n\n", l + b + l + b);
+
+    return;
+
+}
+
+int main(){
+
+    float radius;
+
+    printf("           CIRCLE        \n\n");
+
+    printf("Enter radius of circle : ");
+
+    scanf("%f",&radius);
+
+    circle(radius);
+
+
+
+    float a, b, c, d, e;
+
+    printf("          TRIANGLE         \n\n");
+
+    printf("Enter 1st side of triangle : ");
+
+    scanf("%f", &a);
+
+    printf("Enter 2nd side of triangle : ");
+
+    scanf("%f", &b);
+
+    printf("Enter 3rd side of triangle : ");
+
+    scanf("%f", &c);
+
+    printf("Enter height of triangle : ");
+
+    scanf("%f", &d);
+
+    printf("Enter base of triangle : ");
+
+    scanf("%f", &e);
+
+    triangle(a, b, c, d, e);
+
+
+
+    float f, g;
+
+    printf("          RECTANGLE         \n\n");
+
+    printf("Enter length of rectangle : ");
+
+    scanf("%f", &f);
+
+    printf("Enter bredth of rectangle : ");
+
+    scanf("%f", &g);
+
+    rectangle(f, g);
+
+
+
+    return 0;
+
+}
