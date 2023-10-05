6ff436ec762a0b1fc4547d635caa42c01dd126d4
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 20:21:41 +0530

    Update q4_2.c

--- q4_2.c
+++ q4_2.c
@@ -1,18 +1,25 @@
 #include<stdio.h>
 
-    int main(){
-    int n,sum=0;
-    printf("enter number\n");
-    scanf("%d",&n);
-    for(int j;n!=0;n=n/10){
-         j=n%10;
-         sum+=j;
-    }
+int main(){
 
-  
-   printf("%d",sum);
+	int n;
 
+	printf("ENTER NUMBER : ");
 
+	scanf("%d",&n);
 
-return 0;
-    }
+	int sum=0;
+
+	do{
+
+		sum=sum+n%10;              // ADDING REMAINDER EACH TIME (last digit adding)
+
+		n=n/10;                    // DIVIDING CODE BY 10 TO ADD NEXT DIGIT (removing last digit)
+
+	}while(n>0);                   // LOOP WORK UNTIL N IS GEATER THAN 0 LOOP TERMINATES AT N=0
+
+	printf("THE SUM OF DIGITS OF GIVEN NUMBER IS : %d",sum);
+
+	return 0;
+
+}
