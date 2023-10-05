9ed9e971bb2067d5f45238a8eb7e715a9698a631
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 20:04:33 +0530

    Update q3_2_2.c

--- q3_2_2.c
+++ q3_2_2.c
@@ -1,14 +1,25 @@
 #include<stdio.h>
-//1 5 10 15
-    int main(){
-        int sum=1,N;
-        printf("sum upto?\n");
-        scanf("%d",&N);
-        for(int n=5; n<=N; n=n+5){
-            sum =sum +n;
-        }
-        printf("sum of series=%d",sum);
-    
 
-return 0;
-    }
+int main(){
+
+	int n,i;
+
+	printf("ENTER VALUE OF N : ");
+
+	scanf("%d",&n);
+
+	// SERIES 1 = 1 + 5 + 10 + 15 + ....  UPTO N
+
+	int a=1;                                          // AFTER 1 SERIES IS AN AP SO IT IS TAKEN OUT                          
+
+	for(i=5;i<=n;i=i+5){                             // FIRST 5 IS ADDED THEN 10 UPTO N SO INCREEMENT IN I IS BY 5
+
+		a=a+i;                                      // COMMON DIFFERENCE IS 5  
+
+	}                                               
+
+	printf("THE SOLUTION OF SERIES IS : %d",a); 
+
+	return 0;
+
+}
