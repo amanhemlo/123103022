018e3ef1d79fc188bf746207a9f7d8145aec0c95
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 19:46:08 +0530

    Update q1_3.c

--- q1_3.c
+++ q1_3.c
@@ -1,12 +1,33 @@
 #include<stdio.h>
 
-    int main(){
-    float p,r,t;
+void interest(float amount,float rate,float time){
 
-printf("enter principal, time and rate\n" );
-scanf("%f %f %f",&p,&r,&t);
-float SI=p*r*t/100;
-printf("simple interest =%f",SI);
+	printf("YOUR SIMPLE INTERSET WILL BE : %f\n",(amount*rate*time)/100);
 
-return 0;
-    }
+	return;
+
+}
+
+int main(){
+
+	float a, b, c;
+
+    printf("Enter principle value : ");
+
+    scanf("%f", &a);
+
+    printf("Enter time : ");
+
+    scanf("%f", &b);
+
+    printf("Enter rate : ");
+
+    scanf("%f", &c);
+
+    interest(a, b, c);
+
+
+
+    return 0;
+
+}
