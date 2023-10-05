e857db1ff112a06ffee1fcd4383f2ed249d8641d
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 19:48:57 +0530

    Update q2_2.c

--- q2_2.c
+++ q2_2.c
@@ -1,21 +1,82 @@
-#include<stdio.h>
-void greater(int a, int b, int c){
-    if(a>b && a>c ){
-        printf("a is greatest\n");
-    }
-   else if(b>c && b>a ){
-        printf("b is greatest\n");
-    }
-   else {
-        printf("c is greatest\n");
-    }
-    
-}
-    int main(){
-    int a,b,c;
-    printf("enter 3 numbers\n");
-    scanf("%d %d %d",&a,&b,&c);
-    greater(a,b,c);
 
-return 0;
-    }
+#include<stdio.h>
+
+int main(){
+
+	int a,b,c;
+
+	printf("ENTER FIRST NUMBER : ");
+
+	scanf("%d",&a);
+
+	printf("ENTER SECOND NUMBER : ");    // THREE DISTINCT NUMBER
+
+	scanf("%d",&b);
+
+	printf("ENTER THIRD NUMBER : ");
+
+	scanf("%d",&c);
+
+	
+
+//	if(a>b && a>c){
+
+//		printf("1st NUMBER IS LARGEST");
+
+//	}
+
+//	if(b>c && b>a){
+
+//		printf("2nd NUMBER IS LARGEST");
+
+//	}
+
+//	else{
+
+//		printf("3rd NUMBER IS LARGEST");
+
+//	}
+
+
+
+// IMPROVED CODE WITH IF AND ELSE STATEMENT ONLY
+
+
+
+    if(a>b){
+
+    	if(a>c){
+
+    		printf("1st NUMBER IS LARGEST");      //IF WE ENTER IN THIS STATEMENT MEANS B IS OUT OF RACE
+
+		}                                         //NOW WE HAVE TO CHECK FOR A AND C ONLY 
+
+		else{
+
+			printf("3rd NUMBER IS LARGEST");
+
+		}
+
+	}
+
+
+
+    else{
+
+    	if(b>c){
+
+    		printf("2nd NUMBER IS LARGEST");     //IF WE COME TO THIS STATEMENT MEANS A IS SMALLER THAN B
+
+		}                                        //NOW WE HAVE TO CHECK FOR B AND C ONLY
+
+		else{
+
+			printf("3rd NUMBER IS LARGEST");
+
+		}
+
+	}
+
+	return 0;
+
+}
