96bc55774fc5555257ebe9730465a1cef379a66c
amankumarsharma1309 <146988180+amankumarsharma1309@users.noreply.github.com>
2023-10-05 20:02:28 +0530

    Update q3_2_1.c

--- q3_2_1.c
+++ q3_2_1.c
@@ -1,18 +1,25 @@
-#include <stdio.h>
-main(){
-    
-   
-    
-    int s=1;
-    int n;
-    printf("Enter the value of n \n");
-    scanf("%d",&n);
-    
-    for(int i=1;i<n;i++)
-    s+=(5*i);
+#include<stdio.h>
 
-    printf("sum is %d \n",s);
+int main(){
 
+	int n,i;
 
+	printf("ENTER VALUE OF N : ");
+
+	scanf("%d",&n);
+
+	// SERIES 1 = 1 + 5 + 10 + 15 + ....  UPTO N TERMS
+
+	int a=1;                                          // AFTER 1 SERIES IS AN AP SO IT IS TAKEN OUT                          
+
+	for(i=1;i<n;i++){                             
+
+		a=a+5*i;                                      // COMMON DIFFERENCE IS 5  
+
+	}                                                 // SERIES GOES UPTO N-1 AS 1 IS INCLUDED ABOVE
+
+	printf("THE SOLUTION OF SERIES IS : %d",a); 
+
+	return 0;
 
 }
